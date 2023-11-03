66aa6392182861c11552e73741314b83075ab833
Jesse Strickland <112838688+Skinniez@users.noreply.github.com>
2023-11-03 18:39:27 -0400

    updated spelling

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -25,7 +25,7 @@ const MySkills = () => {
           </p>
           <LineGradient width="w-1/3" />
           <p className="mt-10 mb-7">
-            Full stack software developement with a desire to never stop learning. 
+            Full stack software development with a desire to never stop learning. 
             JavaScript ES6, HTML5, Node.js, Bootstrap, Mocha, Chai, React, Jest, Express.js, Knex.js, MongoDB, Typescript, Photoshop, After Effects, Premiere Pro, Procreate, OBSstudio.
           </p>
         </motion.div>
